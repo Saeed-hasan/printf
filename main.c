fce5f2a50665ff888dd5d413b445c2061230f188
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-09-19 00:46:56 -0700

    main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int len;
 	int len2;
-	
+
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	_printf("Character:[%c]\n", 'H');
@@ -26,5 +26,6 @@ int main(void)
 	printf("Negative:[%d]\n", -762534);
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
+	 _printf("%b\n", 98);
 	return (0);
 }
